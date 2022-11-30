42f889f977a2d3d098c772932a66f127814fdf06
Daniil Naumovets <naumovets.daniil2003@mail.ru>
2022-11-30 04:07:11 +0300

    update SpaceObject

--- src/main/java/spaceObject/SpaceObject.java
+++ src/main/java/spaceObject/SpaceObject.java
@@ -1,8 +1,7 @@
 package spaceObject;
 
 public class SpaceObject {
-//    name,breed,x, y, r, vx, vy, m, color = "black", relativity
-
+    private int id;
     private String name;
     private String breed;
     private double x;
@@ -15,8 +14,24 @@ public class SpaceObject {
     private SpaceObject relativity;
 
 
-    public SpaceObject(String name, String breed, double x, double y, double r, double vx,
+    public SpaceObject(int id,String name, String breed, double x, double y, double r, double vx,
                        double vy, double m, String color, SpaceObject relativity) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+        this.x = x + relativity.getX();
+        this.y = y + relativity.getY();
+        this.r = r;
+        this.vx = vx + relativity.getVx();
+        this.vy = vy + relativity.getVy();
+        this.m = m;
+        this.color = color;
+        this.relativity = relativity;
+    }
+
+    public SpaceObject(int id,String name, String breed, double x, double y, double r,
+                       double vx, double vy, double m, String color) {
+        this.id = id;
         this.name = name;
         this.breed = breed;
         this.x = x;
@@ -26,6 +41,49 @@ public class SpaceObject {
         this.vy = vy;
         this.m = m;
         this.color = color;
-        this.relativity = relativity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public double getVx() {
+        return vx;
+    }
+
+    public double getVy() {
+        return vy;
+    }
+
+    public double getM() {
+        return m;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public SpaceObject getRelativity() {
+        return relativity;
+    }
+
+    public int getId(){
+        return id;
     }
 }
